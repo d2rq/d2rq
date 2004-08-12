70a0555fcb694c61485212ed417eaad1e773c4ec
bizer <bizer>
2004-08-12 09:11:10 +0000

    Fnctional tests included into AllTest.

--- test/de/fuberlin/wiwiss/d2rq/AllTests.java
+++ test/de/fuberlin/wiwiss/d2rq/AllTests.java
@@ -1,10 +1,13 @@
 /*
- * $Id: AllTests.java,v 1.1 2004/08/02 22:48:44 cyganiak Exp $
+ * $Id: AllTests.java,v 1.2 2004/08/12 09:11:10 bizer Exp $
  */
 package de.fuberlin.wiwiss.d2rq;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import de.fuberlin.wiwiss.d2rq.functional_tests.FindTest;
+import de.fuberlin.wiwiss.d2rq.functional_tests.ModelAPITest;
+import de.fuberlin.wiwiss.d2rq.functional_tests.RDQLTest;
 
 /**
  * Test suite for D2RQ
@@ -27,6 +30,9 @@ public class AllTests {
 		suite.addTestSuite(TranslationTableTest.class);
 		suite.addTestSuite(ColumnTest.class);
 		suite.addTestSuite(SQLStatementMakerTest.class);
+		suite.addTestSuite(FindTest.class);
+		suite.addTestSuite(ModelAPITest.class);
+		suite.addTestSuite(RDQLTest.class);
 		//$JUnit-END$
 		return suite;
 	}
