0570d14492d0e19d3e519b3a9fade962cb8931a3
cyganiak <cyganiak>
2006-09-18 11:58:33 +0000

    Set up logging in cmd line scripts

--- dump-rdf.bat
+++ dump-rdf.bat
@@ -1,5 +1,5 @@
 @echo off
-@REM $Id: generate-mapping.bat,v 1.1 2006/09/07 16:57:50 cyganiak Exp $
+@REM $Id: dump-rdf.bat,v 1.1 2006/09/07 20:32:14 cyganiak Exp $
 
 set D2R_ROOT=%0
 pushd %D2R_ROOT%
@@ -12,4 +12,4 @@ if "%CP%" == "" (set CP=%D2R_ROOT%\%1) ELSE (set CP=%CP%;%D2R_ROOT%\%1)
 exit /B
 
 :noMore
-java -cp %CP% d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp "%CP%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
--- generate-mapping.bat
+++ generate-mapping.bat
@@ -1,15 +1,17 @@
 @echo off
-@REM $Id: generate-mapping.bat,v 1.1 2006/05/30 07:29:00 cyganiak Exp $
+@REM $Id: generate-mapping.bat,v 1.1 2006/09/07 16:57:50 cyganiak Exp $
 
-set D2R_ROOT=%0
-pushd %D2R_ROOT%
+set D2RQ_ROOT=%0
+set CP=
+pushd %D2RQ_ROOT%
 for %%f in (lib\*.jar lib\*\*.jar) do call :oneStep %%f
 popd
 goto noMore
 
 :oneStep
-if "%CP%" == "" (set CP=%D2R_ROOT%\%1) ELSE (set CP=%CP%;%D2R_ROOT%\%1)
+if "%CP%" == "" (set CP=%D2RQ_ROOT%\%1) ELSE (set CP=%CP%;%D2RQ_ROOT%\%1)
 exit /B
 
 :noMore
-java -cp %CP% d2rq.generate_mapping %1 %2 %3 %4 %5 %6 %7 %8 %9
+set LOGCONFIG=file:%D2RQ_ROOT%\etc\log4j.properties
+java -cp "%CP%" "-Dlog4j.configuration=%LOGCONFIG%" d2rq.generate_mapping %1 %2 %3 %4 %5 %6 %7 %8 %9
