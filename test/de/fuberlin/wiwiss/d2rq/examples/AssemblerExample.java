f3a4b7921a8fe4cf3cbef7b970945ade0d5c9687
cyganiak <cyganiak>
2006-10-24 12:26:11 +0000

    Added runnable assembler example to CVS, synced with manual

---
+++ test/de/fuberlin/wiwiss/d2rq/examples/AssemblerExample.java
@@ -0,0 +1,23 @@
+package de.fuberlin.wiwiss.d2rq.examples;
+
+import com.hp.hpl.jena.assembler.Assembler;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.util.FileManager;
+
+public class AssemblerExample {
+
+	public static void main(String[] args) {
+		// Load assembler specification from file
+		Model assemblerSpec = FileManager.get().loadModel("doc/example/assembler.n3");
+		
+		// Get the model resource
+		Resource modelSpec = assemblerSpec.createResource(assemblerSpec.expandPrefix(":myModel"));
+		
+		// Assemble a model
+		Model m = Assembler.general.openModel(modelSpec);
+		
+		// Write it to System.out
+		m.write(System.out);
+	}
+}
