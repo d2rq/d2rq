57aacf5e7ce77a485eaeade533240d7a8f912a53
cyganiak <cyganiak>
2006-10-27 10:24:30 +0000

    Fixed Windows batch files

--- dump-rdf.bat
+++ dump-rdf.bat
@@ -1,15 +1,15 @@
 @echo off
-@REM $Id: dump-rdf.bat,v 1.1 2006/09/07 20:32:14 cyganiak Exp $
-
-set D2R_ROOT=%0
-pushd %D2R_ROOT%
-for %%f in (lib\*.jar lib\*\*.jar) do call :oneStep %%f
-popd
-goto noMore
-
-:oneStep
-if "%CP%" == "" (set CP=%D2R_ROOT%\%1) ELSE (set CP=%CP%;%D2R_ROOT%\%1)
+set D2R_ROOT=%~p0
+set CP=%D2RQ_ROOT%build
+call :findjars %D2RQ_ROOT%lib
+set LOGCONFIG=file:%D2RQ_ROOT%etc/log4j.properties
+java -cp "%CP%" "-Dlog4j.configuration=%LOGCONFIG%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
 exit /B
 
-:noMore
-java -cp "%CP%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
+:findjars
+for %%j in (%1\*.jar) do call :addjar %%j
+for /D %%d in (%1\*) do call :findjars %%d
+exit /B
+
+:addjar
+set CP=%CP%;%1
