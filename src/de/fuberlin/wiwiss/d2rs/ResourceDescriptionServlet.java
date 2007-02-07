8b8643b4ccfa33651f7b937c7f32fffc5eafd166
cyganiak <cyganiak>
2007-02-07 22:57:59 +0000

    Added foaf:primaryTopic and rdfs:label triples to all RDF descriptions

--- src/de/fuberlin/wiwiss/d2rs/ResourceDescriptionServlet.java
+++ src/de/fuberlin/wiwiss/d2rs/ResourceDescriptionServlet.java
@@ -10,6 +10,10 @@ import org.joseki.http.ModelResponse;
 
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDFS;
+
+import de.fuberlin.wiwiss.d2rs.vocab.FOAF;
 
 public class ResourceDescriptionServlet extends HttpServlet {
 	
@@ -19,11 +23,24 @@ public class ResourceDescriptionServlet extends HttpServlet {
 			relativeResourceURI = relativeResourceURI + "?" + request.getQueryString();
 		}
 		String resourceURI = D2RServer.instance().resourceBaseURI() + relativeResourceURI;
+		String documentURL = D2RServer.instance().dataURL(relativeResourceURI);
+
 		Model description = QueryExecutionFactory.create(
 				"DESCRIBE <" + resourceURI + ">",
 				D2RServer.instance().dataset()).execDescribe();
 		if (description.size() == 0) {
 			response.sendError(404);
+		}
+		if (description.qnameFor(FOAF.primaryTopic.getURI()) == null
+				&& description.getNsPrefixURI("foaf") == null) {
+			description.setNsPrefix("foaf", FOAF.NS);
+		}
+		Resource resource = description.getResource(resourceURI);
+		Resource document = description.getResource(documentURL);
+		document.addProperty(FOAF.primaryTopic, resource);
+		String label = resource.getProperty(RDFS.label).getString();
+		if (label != null) {
+			document.addProperty(RDFS.label, "RDF Description of " + label);
 		}
 		new ModelResponse(description, request, response).serve();
 //		Resource resource = description.getResource(resourceURI);
