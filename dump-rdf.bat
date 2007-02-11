ef01f84f1b260797cdf3d96175e5ddc725eb275a
cyganiak <cyganiak>
2007-02-11 21:33:28 +0000

    Copied the dump scripts from D2RQ, Chris wants them to be included.

---
+++ dump-rdf.bat
@@ -0,0 +1,15 @@
+@echo off
+set D2R_ROOT=%~p0
+set CP="%D2RQ_ROOT%build"
+call :findjars "%D2RQ_ROOT%lib"
+set LOGCONFIG=file:%D2RQ_ROOT%etc/log4j.properties
+java -cp "%CP%" "-Dlog4j.configuration=%LOGCONFIG%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
+exit /B
+
+:findjars
+for %%j in (%1\*.jar) do call :addjar "%%j"
+for /D %%d in (%1\*) do call :findjars "%%d"
+exit /B
+
+:addjar
+set CP=%CP%;%1
