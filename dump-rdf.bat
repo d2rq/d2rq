59f0c555102c559e903c70aa11d26f59b892054e
cyganiak <cyganiak>
2007-11-04 11:20:27 +0000

    More VM memory for all scripts

--- dump-rdf.bat
+++ dump-rdf.bat
@@ -2,7 +2,7 @@
 set D2R_ROOT=%~p0
 set CP="%D2RQ_ROOT%build"
 call :findjars "%D2RQ_ROOT%lib"
-java -cp "%CP%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp "%CP%" -Xmx256M d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
 exit /B
 
 :findjars
