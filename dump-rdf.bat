7c3bad1d2d241cc939d884e5c5ee5e959cdd0fb2
cyganiak <cyganiak>
2007-11-16 16:51:07 +0000

    Bugfix: Scripts didn't work for windows filenames/dirnames with spaces

--- dump-rdf.bat
+++ dump-rdf.bat
@@ -3,7 +3,7 @@ set D2R_ROOT=%~p0
 set CP="%D2RQ_ROOT%build"
 call :findjars "%D2RQ_ROOT%lib"
 set LOGCONFIG=file:%D2RQ_ROOT%etc/log4j.properties
-java -cp "%CP%" -Xmx256M "-Dlog4j.configuration=%LOGCONFIG%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp %CP% -Xmx256M "-Dlog4j.configuration=%LOGCONFIG%" d2rq.dump_rdf %1 %2 %3 %4 %5 %6 %7 %8 %9
 exit /B
 
 :findjars
