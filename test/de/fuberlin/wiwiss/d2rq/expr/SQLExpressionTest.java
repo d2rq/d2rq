92bc6bc8470d723c6fec987ad8acab3a396dc76c
fatorange <fatorange>
2009-02-07 14:49:27 +0000

    Extended SQLExpressionTest to test for correct handling of quoted text such as URLs

--- test/de/fuberlin/wiwiss/d2rq/expr/SQLExpressionTest.java
+++ test/de/fuberlin/wiwiss/d2rq/expr/SQLExpressionTest.java
@@ -24,9 +24,10 @@ public class SQLExpressionTest extends TestCase {
 	}
 	
 	public void testFindsColumns() {
-		Expression e = SQLExpression.create("papers.publish = 1 AND papers.rating > 4");
+		Expression e = SQLExpression.create("papers.publish = 1 AND papers.url != 'http://www.example.com\\'' AND papers.rating > 4");
 		Set expectedColumns = new HashSet(Arrays.asList(
 				new Attribute[]{new Attribute(null, "papers", "publish"), 
+						new Attribute(null, "papers", "url"),
 						new Attribute(null, "papers", "rating")}));
 		assertEquals(expectedColumns, e.attributes());
 	}
