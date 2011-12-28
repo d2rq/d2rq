b4d6ff5e788ce139382cc08e9ff23b8680cd65cf
jacobus.geluk@gmail.com <jacobus.geluk@gmail.com>
2011-12-28 13:44:51 -0500

    Fixed some more compiler warnings caused by unicode characters in the source

--- src/de/fuberlin/wiwiss/d2rq/map/DynamicProperty.java
+++ src/de/fuberlin/wiwiss/d2rq/map/DynamicProperty.java
@@ -7,7 +7,7 @@ import de.fuberlin.wiwiss.d2rq.algebra.Relation;
 import de.fuberlin.wiwiss.d2rq.pp.PrettyPrinter;
 
 /*
- * @author Jörg Henß
+ * @author Joerg Henss
  * @version $Id: DynamicProperty.java,v 1.3 2009/08/05 11:01:09 fatorange Exp $
  */
 public class DynamicProperty extends ResourceMap {
