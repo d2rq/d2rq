6d73abf1271819d4884ad962dc2751c921bbe7fd
Richard Cyganiak <riccyg@nblion004.(none)>
2012-03-05 10:03:14 +0000

    fix some IE layout issues on website

--- doc/script.js
+++ doc/script.js
@@ -1,5 +1,5 @@
 $(function(){
     $('h2').add('h3').each(function(index, element) {
-        $(element).append('<small> <a class="section-anchor">#</a></small>').find('.section-anchor').attr('href', '#' + element.id);
+        $(element).append('<small>&nbsp;<a class="section-anchor">#</a></small>').find('.section-anchor').attr('href', '#' + element.id);
     });
 });
