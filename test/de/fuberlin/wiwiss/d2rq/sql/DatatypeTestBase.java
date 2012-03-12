69c3094169a8c313f93ddb9986a79773d38d3ced
Richard Cyganiak <richard@cyganiak.de>
2012-03-12 21:46:20 +0000

    refactor to make addition of further database-specific tests easier

---
+++ test/de/fuberlin/wiwiss/d2rq/sql/DatatypeTestBase.java
@@ -0,0 +1,166 @@
+package de.fuberlin.wiwiss.d2rq.sql;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+import de.fuberlin.wiwiss.d2rq.GraphD2RQ;
+import de.fuberlin.wiwiss.d2rq.algebra.RelationName;
+import de.fuberlin.wiwiss.d2rq.dbschema.DatabaseSchemaInspector;
+import de.fuberlin.wiwiss.d2rq.map.ClassMap;
+import de.fuberlin.wiwiss.d2rq.map.Database;
+import de.fuberlin.wiwiss.d2rq.map.Mapping;
+import de.fuberlin.wiwiss.d2rq.map.PropertyBridge;
+
+public abstract class DatatypeTestBase extends TestCase {
+	private final static String EX = "http://example.com/";
+	private final static Resource dbURI = ResourceFactory.createResource(EX + "db");
+	private final static Resource classMapURI = ResourceFactory.createResource(EX + "classmap");
+	private final static Resource propertyBridgeURI = ResourceFactory.createResource(EX + "propertybridge");
+	private final static Resource valueProperty = ResourceFactory.createProperty(EX + "value");
+	
+	private String jdbcURL;
+	private String driver;
+	private String user;
+	private String password;
+	private String schema;
+	
+	private String datatype;
+	private GraphD2RQ graph;
+	private DatabaseSchemaInspector inspector;
+	
+	public void tearDown() {
+		if (graph != null) graph.close();
+	}
+	
+	protected void initDB(String jdbcURL, String driver, 
+			String user, String password, String schema) {
+		this.jdbcURL = jdbcURL;
+		this.driver = driver;
+		this.user = user;
+		this.password = password;
+		this.schema = null;
+	}
+	
+	protected void loadScript(String script) throws FileNotFoundException {
+		ConnectedDB.registerJDBCDriver(driver);
+		ConnectedDB db = new ConnectedDB(jdbcURL, user, password);
+		try {
+			Statement stmt = db.connection().createStatement();
+			try {
+				for (String table: allTables()) {
+					stmt.execute("DROP TABLE " + table);
+				}
+			} finally {
+				stmt.close();
+			}
+			SQLScriptLoader.loadFile(new File(script), db.connection());
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		} finally {
+			db.close();
+		}
+	}
+	
+	protected void createMapping(String datatype) {
+		this.datatype = datatype;
+		Mapping mapping = generateMapping();
+		graph = getGraph(mapping);
+		inspector = mapping.databases().iterator().next().connectedDB().schemaInspector();
+	}
+	
+	protected void assertIsCompleteDatatypeList(String[] tables) {
+		Set<String> expected = new HashSet<String>(Arrays.asList(tables));
+		Set<String> actual = new HashSet<String>();
+		for (String table: allTables()) {
+			if (!table.startsWith("T_")) {
+				fail("Table name doesn't conform to T_{datatype} convention: " + table);
+			}
+			actual.add(table.substring(2));
+		}
+		assertEquals(expected, actual);
+	}
+	
+	protected void assertMappedType(String rdfType) {
+		assertEquals(rdfType, inspector.xsdTypeFor(inspector.columnType(
+				SQL.parseAttribute("T_" + datatype + ".VALUE"))));
+	}
+	
+	protected void assertValues(String[] expectedValues) {
+		assertValues(expectedValues, true);
+	}
+	
+	protected void assertValues(String[] expectedValues, boolean searchable) {
+		ExtendedIterator<Triple> it = graph.find(Node.ANY, Node.ANY, Node.ANY);
+		List<String> listedValues = new ArrayList<String>();
+		while (it.hasNext()) {
+			listedValues.add(it.next().getObject().getLiteralLexicalForm());
+		}
+		assertEquals(Arrays.asList(expectedValues), listedValues);
+		for (String value : expectedValues) {
+			if (searchable) {
+				assertTrue("Expected literal not in graph: '" + value + "'",
+						graph.contains(Node.ANY, Node.ANY, Node.createLiteral(value)));
+			} else {
+				assertFalse("Unexpected literal found in graph: '" + value + "'",
+						graph.contains(Node.ANY, Node.ANY, Node.createLiteral(value)));
+			}
+		}
+	}
+	
+	private Set<String> allTables() {
+		ConnectedDB.registerJDBCDriver(driver);
+		ConnectedDB db = new ConnectedDB(jdbcURL, user, password);
+		try {
+			Set<String> result = new HashSet<String>();
+			inspector = db.schemaInspector();
+			for (RelationName name: inspector.listTableNames(schema)) {
+				result.add(name.toString());
+			}
+			return result;
+		} finally {
+			db.close();
+		}
+	}
+	
+	private GraphD2RQ getGraph(Mapping mapping) {
+		GraphD2RQ result = new GraphD2RQ(mapping);
+		result.getConfiguration().setServeVocabulary(false);
+		result.getConfiguration().setUseAllOptimizations(true);
+		return result;
+	}
+	
+	private Mapping generateMapping() {
+		Mapping mapping = new Mapping();
+		Database database = new Database(dbURI);
+		database.setJDBCDSN(jdbcURL);
+		database.setJDBCDriver(driver);
+		database.setUsername(user);
+		database.setPassword(password);
+		mapping.addDatabase(database);
+		ClassMap classMap = new ClassMap(classMapURI);
+		classMap.setDatabase(database);
+		classMap.setURIPattern("row/@@T_" + datatype + ".ID@@");
+		mapping.addClassMap(classMap);
+		PropertyBridge propertyBridge = new PropertyBridge(propertyBridgeURI);
+		propertyBridge.setBelongsToClassMap(classMap);
+		propertyBridge.addProperty(valueProperty);
+		propertyBridge.setColumn("T_" + datatype + ".VALUE");
+		classMap.addPropertyBridge(propertyBridge);
+		return mapping;
+	}
+}
--- test/de/fuberlin/wiwiss/d2rq/sql/HSQLDBDatatypeTest.java
+++ test/de/fuberlin/wiwiss/d2rq/sql/HSQLDBDatatypeTest.java
@@ -1,26 +1,7 @@
 package de.fuberlin.wiwiss.d2rq.sql;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import junit.framework.TestCase;
-
-import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.graph.Triple;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
-import com.hp.hpl.jena.util.iterator.ExtendedIterator;
-import com.hp.hpl.jena.vocabulary.RDF;
-
 import de.fuberlin.wiwiss.d2rq.D2RQException;
 import de.fuberlin.wiwiss.d2rq.D2RQTestSuite;
-import de.fuberlin.wiwiss.d2rq.GraphD2RQ;
-import de.fuberlin.wiwiss.d2rq.helpers.HSQLDatabase;
-import de.fuberlin.wiwiss.d2rq.map.ClassMap;
-import de.fuberlin.wiwiss.d2rq.map.Database;
-import de.fuberlin.wiwiss.d2rq.map.Mapping;
-import de.fuberlin.wiwiss.d2rq.map.PropertyBridge;
 
 /*
  * TODO: check that querying for some other valid literals doesn't lead to errors
@@ -29,151 +10,238 @@ import de.fuberlin.wiwiss.d2rq.map.PropertyBridge;
  * or generally anything that's a valid XSD literal but not a valid SQL literal of
  * the equivalent type)
  */
-public class HSQLDBDatatypeTest extends TestCase {
-	private final static String EX = "http://example.com/";
-	private final static Resource dbURI = ResourceFactory.createResource(EX + "db");
-	private final static Resource classMapURI = ResourceFactory.createResource(EX + "classmap");
-	private final static Resource propertyBridgeURI = ResourceFactory.createResource(EX + "propertybridge");
-	private final static Resource valueProperty = ResourceFactory.createProperty(EX + "value");
+public class HSQLDBDatatypeTest extends DatatypeTestBase {
 	
-	private HSQLDatabase db;
-	private GraphD2RQ graph;
-	
-	public void setUp() {
-		db = new HSQLDatabase("test");
-		db.executeScript(D2RQTestSuite.DIRECTORY + "sql/hsqldb_datatypes.sql");
+	public void setUp() throws Exception {
+		initDB("jdbc:hsqldb:mem:d2rq_test", "org.hsqldb.jdbcDriver", null, null, null);
+		loadScript(D2RQTestSuite.DIRECTORY + "sql/hsqldb_datatypes.sql");
 	}
 
-	public void tearDown() {
-		db.close(true);
-		if (graph != null) graph.close();
+	public void testAllTables() {
+		assertIsCompleteDatatypeList(new String[]{
+			"TINYINT", "SMALLINT", "INTEGER", "BIGINT",
+			"NUMERIC", "DECIMAL", "DECIMAL_4_2",
+			"DOUBLE", "FLOAT", "REAL",
+			"BOOLEAN",
+			"CHAR_3", "CHAR", "VARCHAR", "LONGVARCHAR", "CLOB",
+			"BINARY_4", "BINARY", "VARBINARY", "LONGVARBINARY", "BLOB",
+			"BIT_4", "BIT", "BIT_VARYING",
+			"DATE",
+			"TIME", "TIME_4", "TIME_TZ", "TIME_TZ_4",
+			"TIMESTAMP", "TIMESTAMP_4", "TIMESTAMP_TZ", "TIMESTAMP_TZ_4",
+			"INTERVAL_DAY", "INTERVAL_HOUR_MINUTE",
+			"OTHER", "ARRAY_INTEGER",			
+		});
 	}
 	
 	public void testTinyInt() {
-		assertValues("TINYINT", new String[]{"0", "1", "-128", "127"});
+		createMapping("TINYINT");
+		assertMappedType("xsd:byte");
+		assertValues(new String[]{"0", "1", "-128", "127"});
 	}
 	
 	public void testSmallInt() {
-		assertValues("SMALLINT", new String[]{"0", "1", "-32768", "32767"});
+		createMapping("SMALLINT");
+		assertMappedType("xsd:short");
+		assertValues(new String[]{"0", "1", "-32768", "32767"});
 	}
 	
 	public void testInteger() {
-		assertValues("INTEGER", new String[]{"0", "1", "-2147483648", "2147483647"});
+		createMapping("INTEGER");
+		assertMappedType("xsd:int");
+		assertValues(new String[]{"0", "1", "-2147483648", "2147483647"});
 	}
 	
 	public void testBigInt() {
-		assertValues("BIGINT", new String[]{"0", "1", "-9223372036854775808", "9223372036854775807"});
+		createMapping("BIGINT");
+		assertMappedType("xsd:long");
+		assertValues(new String[]{"0", "1", "-9223372036854775808", "9223372036854775807"});
 	}
 
+	private final static String[] NUMERIC_VALUES = 
+			{"0", "1", "1000000000000000000", "-1000000000000000000"};
 	public void testNumeric() {
-		String[] expected = new String[]{"0", "1", "1000000000000000000", "-1000000000000000000"};
-		assertValues("NUMERIC", expected);
-		assertValues("DECIMAL", expected);
+		createMapping("NUMERIC");
+		assertMappedType("xsd:decimal");
+		assertValues(NUMERIC_VALUES);
+	}
+	
+	public void testDecimal() {
+		createMapping("DECIMAL");
+		assertMappedType("xsd:decimal");
+		assertValues(NUMERIC_VALUES);
 	}
 	
 	public void testDecimal_4_2() {
-		assertValues("DECIMAL_4_2", new String[]{"0", "1", "4.95", "99.99", "-99.99"});
+		createMapping("DECIMAL_4_2");
+		assertMappedType("xsd:decimal");
+		assertValues(new String[]{"0", "1", "4.95", "99.99", "-99.99"});
 	}
 
+	private final static String[] DOUBLE_VALUES = 
+			{"0.0E0", "1.0E0", "1.0E8", "1.0E-7", "-0.0E0", "INF", "-INF"}; 
 	public void testDouble() {
-		String[] expected = {"0.0E0", "1.0E0", "1.0E8", "1.0E-7", "-0.0E0", "INF", "-INF"}; 
-		assertValues("DOUBLE", expected);
-		assertValues("REAL", expected);
-		assertValues("FLOAT", expected);
+		createMapping("DOUBLE");
+		assertMappedType("xsd:double");
+		assertValues(DOUBLE_VALUES);
+	}
+	
+	public void testReal() {
+		createMapping("REAL");
+		assertMappedType("xsd:double");
+		assertValues(DOUBLE_VALUES);
+	}
+	
+	public void testFloat() {
+		createMapping("FLOAT");
+		assertMappedType("xsd:double");
+		assertValues(DOUBLE_VALUES);
 	}
 	
 	public void testBoolean() {
-		assertValues("BOOLEAN", new String[]{"false", "true"});
+		createMapping("BOOLEAN");
+		assertMappedType("xsd:boolean");
+		assertValues(new String[]{"false", "true"});
 	}
 
 	public void testChar_3() {
-		assertValues("CHAR_3", new String[]{"   ", "AOU", "\u00C4\u00D6\u00DC"});
+		createMapping("CHAR_3");
+		assertMappedType("xsd:string");
+		assertValues(new String[]{"   ", "AOU", "\u00C4\u00D6\u00DC"});
 	}
 	
 	public void testChar() {
-		assertValues("CHAR", new String[]{" ", "A", "\u00C4"});
+		createMapping("CHAR");
+		assertMappedType("xsd:string");
+		assertValues(new String[]{" ", "A", "\u00C4"});
 	}
 	
+	private final static String[] VARCHAR_VALUES = 
+			{"", "   ", "AOU", "\u00C4\u00D6\u00DC"};
 	public void testVarchar() {
-		String[] expected = new String[]{"", "   ", "AOU", "\u00C4\u00D6\u00DC"};
-		assertValues("VARCHAR", expected);
-		assertValues("LONGVARCHAR", expected);
+		createMapping("VARCHAR");
+		assertMappedType("xsd:string");
+		assertValues(VARCHAR_VALUES);
+	}
+	
+	public void testLongvarchar() {
+		createMapping("LONGVARCHAR");
+		assertMappedType("xsd:string");
+		assertValues(VARCHAR_VALUES);
 	}
 	
 	public void testCLOB() {
-		assertValues("CLOB", new String[]{"   ", "AOU", "\u00C4\u00D6\u00DC"});
+		createMapping("CLOB");
+		assertMappedType("xsd:string");
+		assertValues(new String[]{"   ", "AOU", "\u00C4\u00D6\u00DC"});
 	}
 	
 	public void testBinary_4() {
-		assertValues("BINARY_4", new String[] {"00000000", "FFFFFFFF", "F001F001"});
+		createMapping("BINARY_4");
+		assertMappedType("xsd:hexBinary");
+		assertValues(new String[] {"00000000", "FFFFFFFF", "F001F001"});
 	}
 	
 	public void testBinary() {
-		assertValues("BINARY", new String[] {"00", "01", "FF"});
+		createMapping("BINARY");
+		assertMappedType("xsd:hexBinary");
+		assertValues(new String[] {"00", "01", "FF"});
 	}
 	
+	private final static String[] VARBINARY_VALUES = 
+			{"", "00", "01", "F001F001F001F001"};
 	public void testVarBinary() {
-		String[] expected = new String[] {"", "00", "01", "F001F001F001F001"};
-		assertValues("VARBINARY", expected);
-		assertValues("LONGVARBINARY", expected);
+		createMapping("VARBINARY");
+		assertMappedType("xsd:hexBinary");
+		assertValues(VARBINARY_VALUES);
+	}
+	
+	public void testLongVarBinary() {
+		createMapping("LONGVARBINARY");
+		assertMappedType("xsd:hexBinary");
+		assertValues(VARBINARY_VALUES);
 	}
 
 	public void testBLOB() {
-		assertValues("BLOB", new String[] {"00", "01", "F001F001F001F001"});
+		createMapping("BLOB");
+		assertMappedType("xsd:hexBinary");
+		assertValues(new String[] {"00", "01", "F001F001F001F001"});
 	}
 	
 	public void testBit_4() {
-		assertValues("BIT_4", new String[] {"0000", "0001", "1000", "1111"});
+		createMapping("BIT_4");
+		assertMappedType("xsd:string");
+		assertValues(new String[] {"0000", "0001", "1000", "1111"});
 	}
 
 	public void testBit() {
-		assertValues("BIT", new String[] {"0", "1"});
+		createMapping("BIT");
+		assertMappedType("xsd:string");
+		assertValues(new String[] {"0", "1"});
 	}
 
 	public void testBitVarying() {
-		assertValues("BIT_VARYING", new String[] {"", "0", "1", "100000000000000"});
+		createMapping("BIT_VARYING");
+		assertMappedType("xsd:string");
+		assertValues(new String[] {"", "0", "1", "100000000000000"});
 	}
 
 	public void testDate() {
-		assertValues("DATE", new String[] {"0001-01-01", "2012-03-07", "9999-12-31"});
+		createMapping("DATE");
+		assertMappedType("xsd:date");
+		assertValues(new String[] {"0001-01-01", "2012-03-07", "9999-12-31"});
 	}
 
 	public void testTime() {
-		assertValues("TIME", new String[]{"00:00:00", "20:39:21", "23:59:59"});
+		createMapping("TIME");
+		assertMappedType("xsd:time");
+		assertValues(new String[]{"00:00:00", "20:39:21", "23:59:59"});
 	}
 
 	public void testTime_4() {
-		assertValues("TIME_4", new String[]{"00:00:00", "20:39:21.5831", "23:59:59.9999"});
+		createMapping("TIME_4");
+		assertMappedType("xsd:time");
+		assertValues(new String[]{"00:00:00", "20:39:21.5831", "23:59:59.9999"});
 	}
 
 	public void testTimeTZ() {
-		assertValues("TIME_TZ", new String[]{
+		createMapping("TIME_TZ");
+		assertMappedType("xsd:time");
+		assertValues(new String[]{
 				"00:00:00Z", "20:39:21Z", "23:59:59Z", 
 				"20:39:21Z", "20:39:21-01:00", "20:39:21+11:30"});
 	}
 
 	public void testTimeTZ_4() {
-		assertValues("TIME_TZ_4", new String[]{
+		createMapping("TIME_TZ_4");
+		assertMappedType("xsd:time");
+		assertValues(new String[]{
 				"00:00:00Z", "20:39:21.5831Z", "23:59:59.9999Z", 
 				"20:39:21.5831Z", "20:39:21.5831-01:00", "20:39:21.5831+11:30"});
 	}
 	
 	public void testTimestamp() {
-		assertValues("TIMESTAMP", new String[]{
+		createMapping("TIMESTAMP");
+		assertMappedType("xsd:dateTime");
+		assertValues(new String[]{
 				"0001-01-01T00:00:00", 
 				"2012-03-07T20:39:21.583122", 
 				"9999-12-31T23:59:59.999999"});
 	}
 
 	public void testTimestamp_4() {
-		assertValues("TIMESTAMP_4", new String[]{
+		createMapping("TIMESTAMP_4");
+		assertMappedType("xsd:dateTime");
+		assertValues(new String[]{
 				"0001-01-01T00:00:00", 
 				"2012-03-07T20:39:21.5831", 
 				"9999-12-31T23:59:59.9999"});
 	}
 
 	public void testTimestampTZ() {
-		assertValues("TIMESTAMP_TZ", new String[]{
+		createMapping("TIMESTAMP_TZ");
+		assertMappedType("xsd:dateTime");
+		assertValues(new String[]{
 				"0001-01-01T00:00:00Z", 
 				"2012-03-07T20:39:21.583122Z", 
 				"9999-12-31T23:59:59.999999Z", 
@@ -183,7 +251,9 @@ public class HSQLDBDatatypeTest extends TestCase {
 	}
 
 	public void testTimestampTZ_4() {
-		assertValues("TIMESTAMP_TZ_4", new String[]{
+		createMapping("TIMESTAMP_TZ_4");
+		assertMappedType("xsd:dateTime");
+		assertValues(new String[]{
 				"0001-01-01T00:00:00Z", 
 				"2012-03-07T20:39:21.5831Z", 
 				"9999-12-31T23:59:59.9999Z", 
@@ -193,78 +263,35 @@ public class HSQLDBDatatypeTest extends TestCase {
 	}
 
 	public void testIntervalDay() {
-		assertValues("INTERVAL_DAY", new String[]{"0", "1", "99", "-99"}, false);
+		createMapping("INTERVAL_DAY");
+		// TODO Should be mapped to xsd:duration?
+		assertMappedType("xsd:string");
+		assertValues(new String[]{"0", "1", "99", "-99"}, false);
 	}
 
 	public void testIntervalHourMinute() {
-		assertValues("INTERVAL_HOUR_MINUTE", new String[]{
+		createMapping("INTERVAL_HOUR_MINUTE");
+		// TODO Should be mapped to xsd:duration?
+		assertMappedType("xsd:string");
+		assertValues(new String[]{
 				"0:00", "0:01", "1:00", "99:00", "-99:00"}, false);
 	}
 
 	public void testOther() {
-		assertUnmappable("OTHER");
-	}
-	
-	public void testArrayInteger() {
-		assertUnmappable("ARRAY_INTEGER");
-	}
-	
-	private void assertUnmappable(String datatype) {
 		try {
-			graph = getGraph(datatype);
-			graph.find(RDF.Nodes.Alt, RDF.Nodes.Alt, RDF.Nodes.Alt);
-			fail("Should fail due to DATATYPE_UNMAPPABLE for '" + datatype + "'");
+			createMapping("OTHER");
+			fail("Should fail due to DATATYPE_UNMAPPABLE");
 		} catch (D2RQException ex) {
 			assertEquals(D2RQException.DATATYPE_UNMAPPABLE, ex.errorCode());
 		}
 	}
 	
-	private void assertValues(String datatype, String[] expectedValues) {
-		assertValues(datatype, expectedValues, true);
-	}
-	
-	private void assertValues(String datatype, String[] expectedValues, boolean searchable) {
-		graph = getGraph(datatype);
-		ExtendedIterator<Triple> it = graph.find(Node.ANY, Node.ANY, Node.ANY);
-		List<String> listedValues = new ArrayList<String>();
-		while (it.hasNext()) {
-			listedValues.add(it.next().getObject().getLiteralLexicalForm());
+	public void testArrayInteger() {
+		try {
+			createMapping("ARRAY_INTEGER");
+			fail("Should fail due to DATATYPE_UNMAPPABLE");
+		} catch (D2RQException ex) {
+			assertEquals(D2RQException.DATATYPE_UNMAPPABLE, ex.errorCode());
 		}
-		assertEquals(Arrays.asList(expectedValues), listedValues);
-		for (String value : expectedValues) {
-			if (searchable) {
-				assertTrue("Expected literal not in graph: '" + value + "'",
-						graph.contains(Node.ANY, Node.ANY, Node.createLiteral(value)));
-			} else {
-				assertFalse("Unexpected literal found in graph: '" + value + "'",
-						graph.contains(Node.ANY, Node.ANY, Node.createLiteral(value)));
-			}
-		}
-	}
-	
-	private GraphD2RQ getGraph(String datatype) {
-		GraphD2RQ result = new GraphD2RQ(generateMapping(datatype));
-		result.getConfiguration().setServeVocabulary(false);
-		result.getConfiguration().setUseAllOptimizations(true);
-		return result;
-	}
-	
-	private Mapping generateMapping(String datatype) {
-		Mapping mapping = new Mapping();
-		Database database = new Database(dbURI);
-		database.setJDBCDSN(db.getJdbcURL());
-		database.setJDBCDriver("org.hsqldb.jdbcDriver");
-		database.setUsername(db.getUser());
-		mapping.addDatabase(database);
-		ClassMap classMap = new ClassMap(classMapURI);
-		classMap.setDatabase(database);
-		classMap.setURIPattern("row/@@T_" + datatype + ".ID@@");
-		mapping.addClassMap(classMap);
-		PropertyBridge propertyBridge = new PropertyBridge(propertyBridgeURI);
-		propertyBridge.setBelongsToClassMap(classMap);
-		propertyBridge.addProperty(valueProperty);
-		propertyBridge.setColumn("T_" + datatype + ".VALUE");
-		classMap.addPropertyBridge(propertyBridge);
-		return mapping;
 	}
 }
