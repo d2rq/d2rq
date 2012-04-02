14f74603b54abf0e961411bbdb9c8c39203e7117
Richard Cyganiak <richard@cyganiak.de>
2012-04-02 12:44:24 +0100

    add more anchors for crosslinks to D2R Server documentation

--- doc/script.js
+++ doc/script.js
@@ -1,5 +1,11 @@
+function addAnchor(element, id) {
+    $(element).append('<small>&nbsp;<a class="section-anchor">#</a></small>').find('.section-anchor').attr('href', '#' + id);
+}
 $(function(){
-    $('h2').add('h3').add('dt').add('.warning').filter('[id]').each(function(index, element) {
-        $(element).append('<small>&nbsp;<a class="section-anchor">#</a></small>').find('.section-anchor').attr('href', '#' + element.id);
+    $('h2').add('h3').add('h4').add('dt').add('.warning').filter('[id]').each(function(index, element) {
+        addAnchor(element, element.id);
+    });
+    $('.properties th').filter('[id]').each(function(index, element) {
+        addAnchor($(element).next(), element.id);
     });
 });
