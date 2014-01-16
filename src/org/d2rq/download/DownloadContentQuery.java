steinarb/d2rq-maven

addded before/after db cleanup methods to allow for vendor specific behaviour

src/de/fuberlin/wiwiss/d2rq/download/DownloadContentQuery.java
@@ -49,6 +49,7 @@ public class DownloadContentQuery {
 	private ResultSet resultSet = null;
 	private InputStream resultStream = null;
 	private String mediaType = null;
+	private ConnectedDB db = null;
 	
 	/**
 	 * @param downloadMap The download map to be queried
@@ -75,9 +76,15 @@ public class DownloadContentQuery {
 	
 	public void close() {
 		try {
+			if (this.db != null) {
+				this.db.vendor().beforeClose(db.connection());
+			}
 			if (this.statement != null) {
 				this.statement.close();
 				this.statement = null;
+			}
+			if (this.db != null) {
+				this.db.vendor().afterClose(db.connection());
 			}
 			if (this.resultSet != null) {
 				this.resultSet.close();
@@ -102,12 +109,14 @@ public class DownloadContentQuery {
 		SelectStatementBuilder builder = new SelectStatementBuilder(filteredRelation);
 		String sql = builder.getSQLStatement();
 		int contentColumn = builder.getColumnSpecs().indexOf(downloadMap.getContentDownloadColumn()) + 1;
-    	ConnectedDB db = filteredRelation.database();
+    	db = filteredRelation.database();
 		Connection conn = db.connection();
 		try {
 			statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 			log.debug(sql);
+			db.vendor().beforeQuery(conn);
 			resultSet = statement.executeQuery(sql);
+			db.vendor().afterQuery(conn);
 			if (!resultSet.next()) {
 				close();
 				return;	// 0 results
@@ -129,6 +138,8 @@ public class DownloadContentQuery {
 					ResultRowMap.fromResultSet(resultSet, builder.getColumnSpecs(), db));
 		} catch (SQLException ex) {
 			throw new D2RQException(ex);
+		} finally {
+			
 		}
 	}
 }

d2rq/d2rq

Conflicts:
	build.xml
	src/de/fuberlin/wiwiss/d2rq/sql/vendor/SQL92.java
	src/org/d2rq/download/DownloadContentQuery.java

src/org/d2rq/download/DownloadContentQuery.java
@@ -44,6 +44,7 @@ public class DownloadContentQuery {
 	private ResultSet resultSet = null;
 	private InputStream resultStream = null;
 	private String mediaType = null;
+	private SQLConnection db = null;
 	
 	/**
 	 * @param downloadRelation The download map to be queried
@@ -77,9 +78,15 @@ public class DownloadContentQuery {
 	
 	public void close() {
 		try {
+			if (this.db != null) {
+				this.db.vendor().beforeClose(db.connection());
+			}
 			if (this.statement != null) {
 				this.statement.close();
 				this.statement = null;
+			}
+			if (this.db != null) {
+				this.db.vendor().afterClose(db.connection());
 			}
 			if (this.resultSet != null) {
 				this.resultSet.close();
@@ -97,7 +104,7 @@ public class DownloadContentQuery {
 		if (OpUtil.isEmpty(r.getBaseTabular())) return;
 		r = NodeRelationUtil.project(r, DownloadRelation.HEADER);
 		r = NodeRelationUtil.limit(r, 1);
-		SQLConnection db = r.getSQLConnection();
+		db = r.getSQLConnection();
 		SelectStatementBuilder builder = new SelectStatementBuilder(r.getBaseTabular(), db.vendor());
 		String sql = builder.getSQL();
 		int contentColumn = builder.getColumns().indexOf(downloadRelation.getContentDownloadColumn()) + 1;
@@ -105,7 +112,9 @@ public class DownloadContentQuery {
 		try {
 			statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
 			log.debug(sql);
+			db.vendor().beforeQuery(conn);
 			resultSet = statement.executeQuery(sql);
+			db.vendor().afterQuery(conn);
 			if (!resultSet.next()) {
 				close();
 				return;	// 0 results